1d01afd45bf09db523c79731391319e338ab3a29
llhe <helinglin@julanling.com>
2020-11-25 14:00:10 +0800

    feat: 正则完善

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# reg-collect README
-常用代码片段收藏
+# README
+常用正则收藏
+⚾ 目前已收集的正则均来自于fee-helper的正则小组件<br/>
+
+## 用法
+用于vue文件，html文件、ts文件、js文件。在文件中输入`ax`即可查看所有的正则啦
+
+![用法](./image/use.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -9,18 +9,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "reg-collect" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('reg-collect.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ax-reg-collect!');
+	// 查看所有正则
+	let regCommand = vscode.commands.registerCommand('reg-collect.ax-reg', () => {
+		vscode.window.showInformationMessage('输入ax即可查看正则');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(regCommand);
 }
 
 // this method is called when your extension is deactivated
